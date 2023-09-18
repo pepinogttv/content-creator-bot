4ebdc6eb24d05b9803f3af6e3a995687a1ddaa65
simon.blanco <sr.pepinogalactico@gmail.com>
2023-09-18 06:59:28 -0300

    change computer

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ const path = require("path");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
 const {
-  downloadYoutubeTranscription,
+  downloadYoutubeTranscript,
+  alternativeGetTranscript,
 } = require("./services/download-transcriptions");
 const { downloadYotubeVideo } = require("./services/download-video");
 const { splitVideo } = require("./services/split-video");
-
-fs.mkdirSync(path.resolve("tmp", "audios"), { recursive: true });
-fs.mkdirSync(path.resolve("tmp", "transcriptions"), { recursive: true });
+const { randomUUID } = require("crypto");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -32,18 +31,121 @@ function getYoutubeUrl() {
   });
 }
 
+function getCaptionTrackByLenguage(captionTracks) {
+  return new Promise((resolve) => {
+    const languages = captionTracks.map((captionTrack) => {
+      const { languageCode, name } = captionTrack;
+      return { languageCode, name };
+    });
+
+    languages.forEach(({ languageCode, name }, i) => {
+      console.log(`${i + 1}. ${name.simpleText} (${languageCode})`);
+    });
+
+    rl.question("Ingresa el numero del lenguaje: ", (languageNumber) => {
+      const captionTrack = captionTracks[languageNumber - 1];
+      if (!captionTrack) {
+        console.log("Lenguaje invalido");
+        getLanguage(captionTracks).then(resolve);
+      } else {
+        resolve(captionTrack);
+      }
+    });
+  });
+}
+
 main();
 async function main() {
   const youtubeUrl = await getYoutubeUrl();
-  const { videoPath, folderName } = await downloadYotubeVideo(youtubeUrl);
-  splitVideo({
-    inputFile: videoPath,
-    folderName,
-    onFinish: async (result) => {
-      console.log(result);
-    },
-    onSplit: async (result) => {
-      console.log(result);
-    },
-  });
+  const info = await ytdl.getInfo(youtubeUrl);
+
+  const { captionTracks } =
+    info.player_response.captions.playerCaptionsTracklistRenderer;
+
+  const captionTrack = await getCaptionTrackByLenguage(captionTracks);
+
+  console.log(captionTrack.url);
+
+  const [{ videoPath }, transcript] = await Promise.all([
+    new Promise((r) => r({ videoPath: "video.mp4" })),
+    // downloadYotubeVideo(youtubeUrl),
+    downloadYoutubeTranscript(captionTrack.baseUrl),
+  ]);
+
+  const groupByStart = (data) => {
+    let result = [];
+    let temp = [];
+    let lastEndTime = 0;
+
+    for (let item of data) {
+      if (item.start - lastEndTime >= 60) {
+        result.push(temp);
+        temp = [];
+      }
+
+      temp.push(item);
+      lastEndTime = item.start;
+    }
+
+    if (temp.length > 0) {
+      result.push(temp);
+    }
+
+    return result;
+  };
+
+  // splitVideo({
+  //   inputFile: videoPath,
+  //   folderName,
+  //   onFinish: async (result) => {
+  //     console.log(result);
+  //   },
+  //   onSplit: async (result) => {
+  //     console.log(result);
+  //   },
+  // });
 }
+
+// const { searchParams } = new URL(youtubeUrl);
+// const {
+//   videoDetails: {
+//     author: { user },
+//     videoId,
+//   },
+// } = await ytdl.getBasicInfo(youtubeUrl);
+
+// async function getAlternativeTranscriptInfo(youtubeUrl) {
+//   const { chromium } = require("playwright");
+//   const browser = await chromium.launch({ headless: false });
+//   const page = await browser.newPage();
+//   await page.goto(youtubeUrl);
+
+//   await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 30));
+
+//   const btn = await page.$("#button-shape");
+//   await btn.click();
+
+//   await new Promise((resolve) => setTimeout(resolve, 1000));
+//   await page.$('text="Mostrar transcripción"');
+
+//   const result = await page.evaluate(() => {
+//     return [
+//       ...document.querySelector(
+//         ".ytd-transcript-search-panel-renderer tp-yt-paper-listbox"
+//       ).children,
+//     ]
+//       .filter((el) => el instanceof HTMLAnchorElement)
+//       .map((a) => {
+//         const text = a.querySelector("tp-yt-paper-item-body").innerText;
+//         const data = a.querySelector("yt-reload-continuation").data;
+//         return {
+//           text,
+//           data,
+//         };
+//       });
+//   });
+
+//   console.log(result);
+
+//   await browser.close();
+// }
--- src/services/download-transcriptions.js
+++ src/services/download-transcriptions.js
@@ -1,33 +1,108 @@
 const ytdl = require("ytdl-core");
-const fs = require("fs");
-const { validateURL } = require("../utils/ytdl.utils");
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 const { xml2js } = require("xml-js");
 
-xml2js;
+async function getTranscriptEndpointInfo(info) {
+  let panel = info.response.engagementPanels.find(
+    ({ engagementPanelSectionListRenderer }) => {
+      return (
+        engagementPanelSectionListRenderer.panelIdentifier ===
+        "engagement-panel-searchable-transcript"
+      );
+    }
+  );
+  panel = panel.engagementPanelSectionListRenderer;
+  const { continuationEndpoint } = panel.content.continuationItemRenderer;
 
-async function downloadYoutubeTranscription(url) {
-  const { searchParams } = validateURL(url);
-  const video_id = searchParams.get("v");
-
-  const info = await ytdl.getInfo(url);
-
-  const transcriptionURL =
-    info.player_response.captions.playerCaptionsTracklistRenderer
-      .captionTracks[0].baseUrl;
-
-  const { data } = await axios.get(transcriptionURL);
-  const opts = {
-    compact: true,
+  const { apiUrl } = continuationEndpoint.commandMetadata.webCommandMetadata;
+  const endpointBody = {
+    ...continuationEndpoint.getTranscriptEndpoint,
+    context: {
+      client: {
+        hl: "es",
+        gl: "AR",
+        clientName: "WEB",
+        clientVersion: "2.20230824.06.00",
+      },
+    },
   };
+  return { apiUrl, endpointBody };
+}
 
-  const { transcript } = xml2js(data, opts);
+async function alternativeGetTranscript(info, baseUrl) {
+  const { apiUrl, endpointBody } = await getTranscriptEndpointInfo(info);
+  endpointBody.params =
+    "CgtFSHVjVktqMU9RRRISQ2dOaGMzSVNBbVZ6R2dBJTNEGAEqM2VuZ2FnZW1lbnQtcGFuZWwtc2VhcmNoYWJsZS10cmFuc2NyaXB0LXNlYXJjaC1wYW5lbDABOAFAAA%3D%3D";
+  const fullUrl = `${baseUrl}${apiUrl}`;
+  const { data } = await axios.post(fullUrl, endpointBody);
+  return data.actions[0].updateEngagementPanelAction.content.transcriptRenderer.content.transcriptSearchPanelRenderer.body.transcriptSegmentListRenderer.initialSegments.map(
+    (segment) => {
+      const { startMs, endMs, snippet } = segment.transcriptSegmentRenderer;
+      return {
+        startMs: Number(startMs),
+        endMs: Number(endMs),
+        text: snippet.runs[0].text,
+      };
+    }
+  );
+}
 
-  console.log(transcript.text[0]);
-  console.log(transcript.text[1]);
-  console.log(transcript.text[2]);
+async function xmlGetTranscript(xmlUrl) {
+  const { data } = await axios.get(xmlUrl);
+  const { transcript } = xml2js(data, { compact: true });
+  return transcript.text.map((segment) => {
+    return {
+      start: Number(segment._attributes.start),
+      dur: Number(segment._attributes.dur),
+      text: segment._text,
+    };
+  });
+}
+
+// jsdoc notation of downloadYoutubeTranscript
+/**
+ * @typedef {Object} TranscriptSegment
+ * @property {number} start
+ * @property {number} end
+ * @property {string} text
+ *
+ * @param {string} url
+ * @returns {Promise<TranscriptSegment[]>}
+ * */
+
+async function downloadYoutubeTranscript(url) {
+  return xmlGetTranscript(url);
+}
+
+const formatTime = (s) => {
+  const date = new Date(s * 1000);
+  const hh = date.getUTCHours().toString().padStart(2, "0");
+  const mm = date.getUTCMinutes().toString().padStart(2, "0");
+  const ss = date.getUTCSeconds().toString().padStart(2, "0");
+  return `${hh}:${mm}:${ss}`;
+};
+
+function transcriptToSRT(transcript) {
+  return transcript.reduce((acc, curr, i) => {
+    const { startMs, endMs, text } = curr;
+
+    let startS = startMs / 1000;
+    let endS = endMs / 1000;
+
+    startS = Number(startS.toFixed(3));
+    endS = Number(endS.toFixed(3));
+
+    startS = formatTime(startS);
+    endS = formatTime(endS);
+
+    return `${acc}${i + 1}\n${startS} --> ${endS}\n${text}\n\n`;
+  }, "");
 }
 
 module.exports = {
-  downloadYoutubeTranscription,
+  transcriptToSRT,
+  downloadYoutubeTranscript,
+  alternativeGetTranscript,
 };
